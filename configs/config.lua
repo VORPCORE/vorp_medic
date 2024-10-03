202dfaff4d155b2455704d46c264bff0178996cd
RSG -Realstonergamer <xanten11@gmail.com>
2024-10-03 05:22:38 -0400

    Chore: fix commands to be for doctor

--- configs/config.lua
+++ configs/config.lua
@@ -9,8 +9,8 @@ Config.Lang = "English"                   -- language you want to use please mak
 Config.AllowOnlyDeadToAlert = true        -- if true only dead players can alert doctors
 
 Config.AlertDoctorCommand = "alertDoctor" -- command to alert doctors
-Config.cancelalert = "cancelpolicealert"  -- command to cancel alert
-Config.finishalert = "finishpolicelert"   -- command to finish alert
+Config.cancelalert = "cancelDoctorAlert"  -- command to cancel alert
+Config.finishalert = "finishDoctorAlert"   -- command to finish alert
 Config.DoctorMenuCommand = 'doctormenu'   -- Command to go on duty and teleport
 
 -- add any job names here
